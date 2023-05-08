ad8e2358248ae1ea9c0969882f25c587927e7f9e
Dilan De La Rosa <dylandaniel1997@gmail.com>
2023-05-08 00:58:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const typed = new Typed('.typed',{
+        stringsElement:"#text",
+        smartBackspace:true,
+        typeSpeed: 100,
+        backSpeed:100,
+        startDelay:1000,
+        backDelay:1000,
+        loop:true,
+        contentType:'html',
+        cursorChar: '💗',
+})
+
+// const typeted = new Typed('.typeted',{
+//     stringsElement:"#text_blur",
+//     smartBackspace:true,
+//     typeSpeed: 100,
+//     backSpeed:100,
+//     startDelay:900,
+//     backDelay:1000,
+//     loop:true,
+//     contentType:'html',
+//     showCursor:false
+// })
